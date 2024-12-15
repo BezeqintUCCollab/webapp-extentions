bbdf0e4a3e4a4c148516dd0323f5eb1f02ae48c3
BezeqintUCCollab <drorshlomi@gmail.com>
2024-12-15 15:54:55 +0200

    Update install-azurecli.ps1

--- scripts/install-azurecli.ps1
+++ scripts/install-azurecli.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi
-Start-Process msiexec.exe -ArgumentList '/I AzureCLI.msi /quiet' -NoNewWindow -Wait
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile c:\temp\AzureCLI.msi
+Start-Process msiexec.exe -ArgumentList '/I c:\AzureCLI.msi /quiet' -NoNewWindow -Wait
